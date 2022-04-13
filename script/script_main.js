f7ece8bb5744459c6dd91993cd02201d3403e860
SleptsovKonstantin <sleptsov.exceedteam@gmail.com>
2022-04-13 17:23:32 +0300

    add main page

--- script/script_main.js
+++ script/script_main.js
@@ -8,10 +8,29 @@ let valueInputDoctor = "";
 let valueInputDate = "";
 let valueInputComp = "";
 
-window.onload = async() => {
+const dateYesterday = new Date();
+const formatDate = (date) => {
+  let dd = date.getDate();
+
+  if (dd < 10) dd = "0" + dd;
+
+  let mm = date.getMonth() + 1;
+
+  if (mm < 10) mm = "0" + mm;
+
+  let yy = date.getFullYear();
+
+  if (yy < 10) yy = "0" + yy;
+
+  return dd + "." + mm + "." + yy;
+};
+const newDateFormate = formatDate(dateYesterday);
+console.log(newDateFormate);
+
+window.onload = async () => {
   inputName = document.getElementById("inputName");
   inputName.addEventListener("change", updateValueName);
-  
+
   inputDoctor = document.getElementById("select");
   inputDoctor.addEventListener("change", updateValueDoctor);
 
@@ -27,7 +46,7 @@ window.onload = async() => {
   const data = await resp.json();
   arrayRecords = data;
   console.log(arrayRecords);
-  // render();
+  render();
 };
 
 const updateValueName = (event) => {
@@ -40,6 +59,7 @@ const updateValueDoctor = (event) => {
 
 const updateValueDate = (event) => {
   valueInputDate = event.target.value.trim();
+  console.log(valueInputDate);
 };
 
 const updateValueComp = (event) => {
@@ -52,7 +72,12 @@ const exitPage = () => {
 };
 
 const addRecord = async () => {
-  if (valueInputName && valueInputDoctor && valueInputDate && valueInputComp) {
+  if (
+    valueInputName &&
+    valueInputDoctor &&
+    (valueInputDate || newDateFormate) &&
+    valueInputComp
+  ) {
     const resp = await fetch("http://localhost:8000/api/records/createRecord", {
       method: "POST",
       headers: {
@@ -63,7 +88,7 @@ const addRecord = async () => {
       body: JSON.stringify({
         name: valueInputName,
         doctor: valueInputDoctor,
-        data: valueInputDate,
+        data: valueInputDate || newDateFormate,
         complaint: valueInputComp,
       }),
     });
@@ -74,7 +99,85 @@ const addRecord = async () => {
     valueInputDoctor = "";
     valueInputDate = "";
     valueInputComp = "";
+    render();
   } else {
     alert("Введены не все данные!");
   }
+};
+
+const render = () => {
+  const total = document.getElementById("fields");
+  const content = document.getElementById("records");
+
+  while (content.firstChild) {
+    content.removeChild(content.firstChild);
+  }
+
+  while (total.firstChild) {
+    total.removeChild(total.firstChild);
+  }
+  // ------------- up
+  const sortRecord = document.createElement("div");
+  sortRecord.id = "sortRecord";
+  const sortStr = document.createElement("p");
+  sortStr.innerText = `Сортировать по: `;
+
+  const inputSort = document.createElement("select");
+  inputSort.id = "inputSort";
+  inputSort.size = "1";
+  const optionOne = document.createElement("option");
+  optionOne.innerText = ``;
+  optionOne.value = `id`;
+  const optionTwo = document.createElement("option");
+  optionTwo.innerText = `Имя`;
+  optionTwo.value = `name`;
+  const optionThree = document.createElement("option");
+  optionThree.innerText = `Врач`;
+  optionThree.value = `doctor`;
+  const optionFour = document.createElement("option");
+  optionFour.innerText = `Дата`;
+  optionFour.value = `data`;
+
+  inputSort.appendChild(optionOne);
+  inputSort.appendChild(optionTwo);
+  inputSort.appendChild(optionThree);
+  inputSort.appendChild(optionFour);
+  sortRecord.appendChild(sortStr);
+  sortRecord.appendChild(inputSort);
+
+  total.appendChild(sortRecord);
+  // ------------------- down table
+
+  const upTable = document.createElement("div");
+  upTable.id = "upTable";
+  const nameTable = document.createElement("p");
+  nameTable.innerText = "Имя";
+  nameTable.className = "fieldTableName";
+  const doctorTable = document.createElement("p");
+  doctorTable.innerText = "Врач";
+  doctorTable.className = "fieldTableDoc";
+  const dateTable = document.createElement("p");
+  dateTable.innerText = "Дата";
+  dateTable.className = "fieldTableDate";
+  const compTable = document.createElement("p");
+  compTable.innerText = "Жалобы";
+  compTable.className = "fieldTableComp";
+  const noTable = document.createElement("p");
+  noTable.innerText = "  ";
+  noTable.className = "fieldTableNo";
+
+  upTable.appendChild(nameTable);
+  upTable.appendChild(doctorTable);
+  upTable.appendChild(dateTable);
+  upTable.appendChild(compTable);
+  upTable.appendChild(noTable);
+  content.appendChild(upTable);
+
+  const table = document.createElement("table");
+
+  arrayRecords.map((item, index) => {
+    const line = document.createElement("tr");
+    
+
+  });
 };
